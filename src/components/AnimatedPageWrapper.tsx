c9e1b67eb0ee5067098ddcae7a73bc35eb35757c
Pratham <pranka0789@gmail.com>
2025-07-16 16:00:44 +0530

    changed duration of fade in when clicked on team

--- src/components/AnimatedPageWrapper.tsx
+++ src/components/AnimatedPageWrapper.tsx
@@ -12,7 +12,7 @@ export default function AnimatedPageWrapper({ children }: { children: React.Reac
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 1.5 }}
+        transition={{ duration: 0.6 }}
       >
         {children}
       </motion.div>
