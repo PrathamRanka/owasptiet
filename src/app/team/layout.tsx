10e72c8db1c7a53154ca1e3b2b277287d16057ed
Pratham <pranka0789@gmail.com>
2025-07-16 10:30:45 +0530

    removed lenis

--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useLenis } from "@/lib/useLenis";
 import { ShootingStars } from "@/ui/shooting-stars";
 import { StarsBackground } from "@/ui/stars-background";
 import AnimatedPageWrapper from "@/components/AnimatedPageWrapper";
 
 export default function TeamLayout({ children }: { children: React.ReactNode }) {
-  useLenis();
 
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden bg-black text-white">
