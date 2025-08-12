2cd8dfbf2485b6484cc645852fa2a68e6e19aa57
Pratham <pranka0789@gmail.com>
2025-08-13 01:12:15 +0530

    added aniamted page wrapper

---
+++ src/components/AnimatedPageWrapper.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { motion, AnimatePresence } from "framer-motion"
+import { usePathname } from "next/navigation"
+
+export default function AnimatedPageWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname} // changes when route changes
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
