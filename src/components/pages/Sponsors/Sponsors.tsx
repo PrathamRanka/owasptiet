990e2365c66fb349e2b7128184f1276a6622cd8a
Pratham <pranka0789@gmail.com>
2025-08-18 16:23:52 +0530

    updated sponsors photo

--- src/components/pages/Sponsors/Sponsors.tsx
+++ src/components/pages/Sponsors/Sponsors.tsx
@@ -12,20 +12,20 @@ export function Sponsors() {
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505363/paytm_l8xl0w.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505363/redbull_tugn5g.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505363/jio_ly5sdu.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/polygon_hqf4pp.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/mlh_cwz3hn.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505361/github_yks7aa.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505361/pepsi_r5kbk6.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505361/ibm_xepg3z.jpg",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755514358/ChatGPT_Image_Aug_18_2025_04_22_09_PM_wkscf8.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513229/mlh_hacking_edfbnt.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513079/github_odc7w9.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513037/pepsi_c0peu5.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513242/ibm_ayrgb3.jpg",
   ], []);
 
   const associateSponsors = useMemo(() => [
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/blockseblock_logo_u6wicm.jpg",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505361/souled_jk9hux.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513229/souled_tjlypp.jpg",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/solana_t7pfvp.jpg",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/replit_tspfxb.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513025/replit_pmnnmg.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/postman_m6rzp3.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/eth_tnpx2h.svg",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505361/eth_tnpx2h.svg",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/devfolio_thzxdq.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/codechef_bdzsdh.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/codingblocks_f3i7lj.png",
@@ -33,13 +33,12 @@ export function Sponsors() {
 
   const communityPartners = useMemo(() => [
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505363/jawed-habib_ihfrno.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/starbucks_nd1yeb.jpg",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/tim-horotns_pnnqnl.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/xyz_slcx2i.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/belgian_waffle-removebg-preview_kcvvt4.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505360/archives-removebg-preview_svslrb.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505359/dev-removebg-preview_ms1yd9.png",
-    "https://res.cloudinary.com/dduzorsii/image/upload/v1755505359/starbucks-removebg-preview_uzxdm0.png",
+    "https://res.cloudinary.com/dduzorsii/image/upload/v1755513068/satrbucks-removebg-preview_tykdek.png",
     "https://res.cloudinary.com/dduzorsii/image/upload/v1755505362/codingninjas_vwku0y.png",
   ], []);
 
@@ -55,7 +54,7 @@ export function Sponsors() {
             exit={{ opacity: 0, scale: 0.8, boxShadow: "0 0 0px rgba(0,0,0,0)" }}
             transition={{ duration: 0.5, delay: i * 0.1 }}
           >
-            <div className="h-20 w-32 sm:h-28 sm:w-44 md:h-32 md:w-52 lg:h-36 lg:w-56 xl:h-40 xl:w-60 relative">
+            <div className="h-24 w-32 sm:h-28 sm:w-44 md:h-32 md:w-52 lg:h-32 lg:w-52 xl:h-36 xl:w-56 relative">
               <Image
                 src={imgUrl}
                 alt={`logo_${i}`}
