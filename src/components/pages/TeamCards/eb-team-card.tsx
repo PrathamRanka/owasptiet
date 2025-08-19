817bba5320c0f1bb9611510f9b5f337fffdb1270
Pratham <pranka0789@gmail.com>
2025-08-19 14:36:15 +0530

    updated eb

--- src/components/pages/TeamCards/eb-team-card.tsx
+++ src/components/pages/TeamCards/eb-team-card.tsx
@@ -59,7 +59,7 @@ const teamCards: Card[] = [
   {
     title: "Amritesh Bharadwaj",
     designation: "TECHNICAL SECRETARY",
-    src: "https://res.cloudinary.com/dduzorsii/image/upload/v1755550315/amritesh_lfyhk1.png",
+    src: "https://res.cloudinary.com/dduzorsii/image/upload/v1755594335/Amritesh_a6r6ir.jpg",
     linkedin: "https://www.linkedin.com/in/amritesh-bhardwaj/",
   },
   {
